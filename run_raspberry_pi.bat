059c50d7e582d4d7dfafc3e47cefb4da9e74a6e1
MrDiagnose <johndripper5@gmail.com>
2021-04-16 00:51:50 +0530

    uploaded run_raspberry_pi.bat

---
+++ run_raspberry_pi.bat
@@ -0,0 +1,12 @@
+"C:\Program Files\qemu\qemu-system-aarch64.exe" ^
+-M raspi3b ^
+-append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" ^
+-dtb bcm2710-rpi-3-b-plus.dtb ^
+-sd 2021-01-11-raspios-buster-armhf.img ^
+-kernel kernel8.img ^
+-m 1G ^
+-smp 4 ^
+-serial stdio ^
+-usb ^
+-device usb-mouse ^
+-device usb-kbd ^
